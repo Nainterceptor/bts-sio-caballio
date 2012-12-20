ae2358aeedea442b446df6cefbde5c3dacb1cc73
maxperrimond <max.perrimond@gmail.com>
2012-12-20 15:26:27 +0100

    Création des entités Paiement et TypePaiement, Suppression de Facture et debug de PensionBundle fixes #45, #44

---
+++ src/chev/PensionBundle/Entity/Paiement.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace chev\PensionBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * chev\PensionBundle\Entity\Paiement
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="chev\PensionBundle\Entity\PaiementRepository")
+ */
+class Paiement
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     *
+     * @ORM\ManyToOne(targetEntity="chev\PensionBundle\Entity\TypePaiement")
+     * @ORM\JoinColumn()
+     * @Assert\NotBlank()
+     */
+    private $typePaiement;
+
+    /**
+     * @var \DateTime $datePaiement
+     *
+     * @ORM\Column(name="datePaiement", type="datetime")
+     * @Assert\NotBlank()
+     * @Assert\DateTime()
+     */
+    private $datePaiement;
+
+    /**
+     * @var \DateTime $dateEncaissement
+     *
+     * @ORM\Column(name="dateEncaissement", type="datetime")
+     * @Assert\NotBlank()
+     * @Assert\DateTime()
+     */
+    private $dateEncaissement;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="montant", type="float")
+     * @Assert\NotBlank()
+     * @Assert\Type(type="float", message="Ce montant : {{ value }} ,n'est pas un {{ type }}.")
+     */
+    private $montant;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set datePaiement
+     *
+     * @param \DateTime $datePaiement
+     * @return Paiement
+     */
+    public function setDatePaiement($datePaiement)
+    {
+        $this->datePaiement = $datePaiement;
+    
+        return $this;
+    }
+
+    /**
+     * Get datePaiement
+     *
+     * @return \DateTime 
+     */
+    public function getDatePaiement()
+    {
+        return $this->datePaiement;
+    }
+
+    /**
+     * Set dateEncaissement
+     *
+     * @param \DateTime $dateEncaissement
+     * @return Paiement
+     */
+    public function setDateEncaissement($dateEncaissement)
+    {
+        $this->dateEncaissement = $dateEncaissement;
+    
+        return $this;
+    }
+
+    /**
+     * Get dateEncaissement
+     *
+     * @return \DateTime 
+     */
+    public function getDateEncaissement()
+    {
+        return $this->dateEncaissement;
+    }
+
+    /**
+     * Set montant
+     *
+     * @param float $montant
+     * @return Paiement
+     */
+    public function setMontant($montant)
+    {
+        $this->montant = $montant;
+    
+        return $this;
+    }
+
+    /**
+     * Get montant
+     *
+     * @return float 
+     */
+    public function getMontant()
+    {
+        return $this->montant;
+    }
+
+    /**
+     * Set typePaiement
+     *
+     * @param \chev\PensionBundle\Entity\TypePaiement $typePaiement
+     * @return Paiement
+     */
+    public function setTypePaiement(\chev\PensionBundle\Entity\TypePaiement $typePaiement = null)
+    {
+        $this->typePaiement = $typePaiement;
+    
+        return $this;
+    }
+
+    /**
+     * Get typePaiement
+     *
+     * @return \chev\PensionBundle\Entity\TypePaiement 
+     */
+    public function getTypePaiement()
+    {
+        return $this->typePaiement;
+    }
+}

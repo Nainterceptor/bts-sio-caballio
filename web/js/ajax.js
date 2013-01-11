7b04b6b6a954a819038a37363382d826855814b2
Gaël Demette - McBook <demette.gael@gmail.com>
2013-01-11 10:03:48 +0000

    * Modification footer * Création d'un layout pour l'ajax * Modification méthode extends layout
    refs #26

--- web/js/ajax.js
+++ web/js/ajax.js
@@ -1,12 +1,13 @@
 function aInAjax() {
+    $("a").unbind("click");
 	$("a").click(function() {
 		var targetUrl = $(this).attr('href')
 		$.ajax({
 			url : targetUrl, 
 			cache : false, 
 			success : function(html) {
-				$('html').empty();
-				$('html').append(html);
+				$('body > section').empty();
+				$('body > section').append(html);
 				aInAjax();
 				window.history.pushState(document.title,document.title, targetUrl);
 			},

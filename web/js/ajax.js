ce3c5fce08106207e067104b45ae36caff7ceb69
Gael Demette PC <demette.gael@gmail.com>
2013-01-14 22:08:46 +0100

    Ajout d'une case a cocher pour activer/desactiver l'Ajax.
    refs #26

--- web/js/ajax.js
+++ web/js/ajax.js
@@ -1,20 +1,23 @@
 function aInAjax() {
     $("a").unbind("click");
 	$("a").click(function() {
-		var targetUrl = $(this).attr('href')
-		$.ajax({
-			url : targetUrl, 
-			cache : false, 
-			success : function(html) {
-				$('body > section').empty();
-				$('body > section').append(html);
-				aInAjax();
-				window.history.pushState(document.title,document.title, targetUrl);
-			},
-		});
-		return false;
+		if($("div.ajaxOn input#ajaxOn").is(':checked')) {
+			var targetUrl = $(this).attr('href')
+			$.ajax({
+				url : targetUrl, 
+				cache : false, 
+				success : function(html) {
+					$('body > section').empty();
+					$('body > section').append(html);
+					aInAjax();
+					window.history.pushState(document.title,document.title, targetUrl);
+				},
+			});
+			return false;
+		}
 	});
 }
 $(document).ready(function() {
 	aInAjax();
+	$("div.ajaxOn").show();
 });

2147d8363e81fb23a280206b0e58d167bedc16c6
Gaël Demette <demette.gael@gmail.com>
2013-01-31 16:39:24 +0100

    Corrections
    refs #27

--- web/js/datetimepicker.js
+++ web/js/datetimepicker.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var id;
 	$('.datetimepicker').each(function(){
 		id = $(this).attr('id');
-		$('input[type="text"]', this).timepicker({
+		$('input[type="text"]', this).datetimepicker({
 			altField: '#'+id+' input[type="time"]',
 			stepMinute: 5,
 			ampm: true,

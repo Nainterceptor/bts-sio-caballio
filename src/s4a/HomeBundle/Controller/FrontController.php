262e328610fa364fd058dbc7e7bf08510b542d07
Fireaxe <axess9fireaxe@gmail.com>
2013-02-03 23:40:24 +0100

    * Modification du front.
    refs #91

--- src/s4a/HomeBundle/Controller/FrontController.php
+++ src/s4a/HomeBundle/Controller/FrontController.php
@@ -10,6 +10,17 @@ class FrontController extends Controller
     
     public function indexAction()
     {
-        return $this->render('s4aHomeBundle:Front:index.html.twig', array());
+    	$user = $this->get('security.context')->getToken()->getUser();
+		
+		if($this->get('security.context')->isGranted('ROLE_ADMIN'))
+        return $this->render('s4aHomeBundle:Front:indexAdmin.html.twig', array());
+		
+		elseif($this->get('security.context')->isGranted('ROLE_GERANT'))
+		return $this->render('s4aHomeBundle:Front:indexGerant.html.twig', array());
+		
+		elseif($this->get('security.context')->isGranted('ROLE_USER'))
+		return $this->render('s4aHomeBundle:Front:indexUser.html.twig', array());
+		
+		return $this->render('s4aHomeBundle:Front:index.html.twig', array());
     }
 }

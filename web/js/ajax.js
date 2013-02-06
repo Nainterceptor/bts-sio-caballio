45ec2067b9dc6edcdf7ca9887925d4816af8e6d1
Gael Demette PC <demette.gael@gmail.com>
2013-02-07 00:36:43 +0100

    Ameliorations++ ajax refs #99

--- web/js/ajax.js
+++ web/js/ajax.js
@@ -1,12 +1,25 @@
 function refreshContent(html, url) {
-	$('body .span9 .row-fluid').html(html);
+	if(url.match(/login$/)) {
+		$('#blackWait').html(html);
+	} else {
+		$('body .span9').html(html);
+		toggleLoading();
+	}
 	window.history.pushState(document.title, document.title, url);
 	if(url.match(/login_check|logout/) != null) {
+		$.ajax({
+			url : Routing.generate('_menuTop'),
+			cache : false,
+			success : function(html) {
+				$('nav.top').html(html);
+				aInAjax();
+			},
+		});
 		$.ajax({
 			url : Routing.generate('_menu'),
 			cache : false,
 			success : function(html) {
-				$('nav').html(html);
+				$('well sidebar-nav').html(html);
 				aInAjax();
 			},
 		});
@@ -18,10 +31,11 @@ function aInAjax() {
 	$("a").bind("click", function() {
 		if($("div.ajaxOn input#ajaxOn").attr('checked') == 'checked') {
 			var targetUrl = $(this).attr('href')
+			toggleLoading(true);
 			$.ajax({
 				url : targetUrl, 
 				cache : false, 
-				success : function(html) {
+				complete : function(html) {
 					refreshContent(html, targetUrl);
 				},
 			});
@@ -40,11 +54,12 @@ function aInAjax() {
 			if($(this).attr("method") !== undefined) {
 				type = $(this).attr("method");
 			}
+			toggleLoading(true);
 			$.ajax({
 				url: url,
 				data: data,
 				type: type,
-				success : function(html) {
+				complete : function(html) {
 					refreshContent(html, url);
 				}
 			});
@@ -65,6 +80,29 @@ function styleAjax() {
 			$(this).addClass('btn-success');
 		}
 	});
+}
+function toggleBlackGround(force) {
+	force = typeof force !== 'undefined' ? force : false;
+	if(!$('#blackWait').length || force == true) {
+		$('#blackWait').remove();
+		$('body').append('<div id="blackWait"></div>');
+		return true;
+	} else {
+		$('#blackWait').remove();
+		return false;
+	}
+}
+function toggleLoading(force) {
+	force = typeof force !== 'undefined' ? force : false;
+	if(!$('#blackWait .loading').length || force == true) {
+		toggleBlackGround(true);
+		
+		$('#blackWait').html('<div class="loading"></div>');
+		return true;
+	} else {
+		$('#blackWait').remove();
+		return false;
+	}
 }
 $(document).ready(function() {
 	aInAjax();

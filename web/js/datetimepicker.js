d91c71b487b0ea62b197d25fc5fd99f8b135a03a
Gaël Demette <demette.gael@gmail.com>
2013-03-07 12:15:25 +0100

    Corrections datepicker
    fixes #145

--- web/js/datetimepicker.js
+++ web/js/datetimepicker.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
+	datetimepicker();
+});
+
+function datetimepicker() {
 	var id;
-	$('.datetimepicker').each(function(){
+	$('.datetimepicker').each(function(){	
 		id = $(this).attr('id');
 		$('input[type="text"]', this).datetimepicker({
 			altField: '#'+id+' input[type="time"]',
@@ -11,4 +15,7 @@ $(document).ready(function(){
 			dateFormat: "dd/mm/yy"
 		});
 	});
-});
+	$('.datepicker').each(function(){
+		$(this).datepicker();
+	});
+}
